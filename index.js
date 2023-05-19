85dcbb950bb8cee6d39e0523809e0a58448019c9
Masum Billah <masumbillahyusuf1621@gmail.com>
2023-05-19 12:49:58 +0600

    send data by category

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ const app = express();
 const port = process.env.PORT || 5000;
 
 const categories = require("./data/categories.json");
+const allCarToys = require("./data/single_toy.json");
 
 // middleware
 app.use(cors());
@@ -27,6 +28,33 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+
+    const toyCollection = client.db("carToysDB").collection("carToys");
+
+    app.get("/carToys", async (req, res) => {
+      const result = await toyCollection.find().toArray();
+      res.send(result);
+    });
+
+    // load categories data
+    app.get("/categories", (req, res) => {
+      res.send(categories);
+    });
+
+    // load data by category
+    app.get("/categories/:id", (req, res) => {
+      const id = parseInt(req.params.id);
+      console.log(id);
+      if (id === 0) {
+        res.send(allCarToys);
+      } else {
+        const categoryToys = allCarToys.filter(
+          (sub) => parseInt(sub.category_id) === id
+        );
+        res.send(categoryToys);
+      }
+    });
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log(
@@ -38,11 +66,6 @@ async function run() {
   }
 }
 run().catch(console.dir);
-
-// load categories data
-app.get("/categories", (req, res) => {
-  res.send(categories);
-});
 
 // check server is running
 app.get("/", (req, res) => {
